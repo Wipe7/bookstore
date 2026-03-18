72f237b457919df18dd224a143b18649c33232d7
Wipe7 <wpeixototeixeira@gmail.com>
2026-03-18 08:38:34 -0300

    feat: add docker-compose with PostgreSQL and update Django settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-d+gv%@dv+r^ivyppz4pemo+d$5mnh$)sasclqxapu=eu7w0s^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'bookstore'),
+        'USER': os.getenv('DB_USER', 'bookstore_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'bookstore_pass'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
