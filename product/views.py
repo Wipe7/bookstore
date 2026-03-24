240a89ac4ff6b42ab73f92b070e4c5dd3aa789af
Wipe7 <wpeixototeixeira@gmail.com>
2026-03-23 21:32:24 -0300

    fix: remove authentication from Product and Category views

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import AllowAny
 from bookstore.pagination import StandardResultsSetPagination
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -8,11 +10,15 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = StandardResultsSetPagination
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     pagination_class = StandardResultsSetPagination
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         queryset = Product.objects.all()
